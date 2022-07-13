8d52ad9659f23219cbe1dc928710e11f33cda2fb
Natalia Zawadzka <nzawadzka@deloittece.com>
2022-07-13 15:52:58 +0200

    create repository for Agency, Car, CarRent, Client and Worker

---
+++ src/main/java/com/dolittle/demo/carMaintenance/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.dolittle.demo.carMaintenance.repository;
+
+import com.dolittle.demo.carMaintenance.entity.CarEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<CarEntity, Long> {
+}
