7b37630f212a623d7c7605ba7ff10a1230ee3ec2
Natalia Zawadzka <nzawadzka@deloittece.com>
2022-07-13 13:00:16 +0200

    create carRent entities

--- src/main/java/com/dolittle/demo/model/CarRent.java
+++ src/main/java/com/dolittle/demo/model/CarRent.java
@@ -1,10 +1,25 @@
 package com.dolittle.demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-
+@Entity
 public class CarRent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "carRent_id_generator")
+    @SequenceGenerator(name = "carRent_id_generator", sequenceName="carRent_id_sqe", allocationSize = 1)
+    private Long id;
+    private String accountNumber;
+    private String date;
+    private String agencyTaken;
+    private String agencyReturn;
+
+    protected CarRent(){}
+
+    public CarRent(Long id, String accountNumber, String date, String agencyTaken, String agencyReturn) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.date = date;
+        this.agencyTaken = agencyTaken;
+        this.agencyReturn = agencyReturn;
+    }
 }
